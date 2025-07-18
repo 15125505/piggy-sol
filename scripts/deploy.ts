d4ea8bfa3f78b4029d981a30a60362b543d6a562
zhoufeng <15125505@qq.com>
2025-07-18 16:53:34 +0800

    更新部署脚本，修改合约部署为 Piggy，并指定 WLD（ERC20）合约地址。

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-    const StringTestFactory = await ethers.getContractFactory("StringTest");
-    const stest = await StringTestFactory.deploy();
-    await stest.waitForDeployment();
-    console.log("✅ StringTest已部署:", await stest.getAddress());
+    // 指定WLD（ERC20）合约地址
+    const WLD_ADDRESS = "0x2cFc85d8E48F8EAB294be644d9E25C3030863003";
+    const PiggyFactory = await ethers.getContractFactory("Piggy");
+    const Piggy = await PiggyFactory.deploy(WLD_ADDRESS);
+    await Piggy.waitForDeployment();
+    console.log("✅ Piggy已部署:", await Piggy.getAddress());
 }
 
 main().catch((error) => {
